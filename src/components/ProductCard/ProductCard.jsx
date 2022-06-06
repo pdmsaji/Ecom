48cd1af33781326ae642e312bf6b35c3547bbffa
pdmsaji <pdmsajith@gmail.com>
2022-06-06 18:58:44 +0530

    initial commit

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ProductCard.css';
+ 
+const ProductCard = () => {
+    return (
+        <>
+            <div className="main-card-wrapper">
+                <div className="image-wrapper">
+
+                </div>
+                <div className="meta-wrapper">
+                    <h3 className="p-name">womens jacket</h3>
+                    <h2 className="p-name">$ : 24.00</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProductCard;
